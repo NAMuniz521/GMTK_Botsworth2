2ae5e756f95493b75dcde1b84e9237f575a0097e
Michael Black <blackmichael@tamu.edu>
2020-07-10 17:52:57 -0500

    CharacterController
    Added Unity Input system, along with a test scene. Right now player can move left and right

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] Transform playerTransform;
+
+    [Header("Input")]
+    [SerializeField] PlayerInputAction inputActions;
+
+    [Range(0,1)]
+    public float speed;
+
+    //Movement
+    Vector2 moveInput;
+
+
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        playerTransform = GetComponent<Transform>();
+
+        inputActions = new PlayerInputAction();
+        inputActions.Player.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 moveDir = new Vector3(moveInput.x * speed, 0, 0);
+        playerTransform.position += Vector3.Lerp(Vector3.zero, moveDir, Time.deltaTime * 10f);
+
+      
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable();
+    }
+}
---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    static object m_lock = new object();
+
+    protected static T m_singleton;
+    public static T singleton
+    {
+        get
+        {
+            lock (m_lock)
+            {
+                if (m_singleton == null)
+                {
+                    m_singleton = FindObjectOfType<T>();
+
+                    if (m_singleton == null)
+                    {
+                        GameObject newInstanceObject = new GameObject();
+                        newInstanceObject.name = "Singleton[" + typeof(T).ToString() + "]";
+                        m_singleton = newInstanceObject.AddComponent<T>();
+                    }
+                }
+            }
+
+            return m_singleton;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        lock (m_lock)
+        {
+            if (m_singleton == null)
+            {
+                m_singleton = FindObjectOfType<T>();
+
+                if (m_singleton == null)
+                {
+                    GameObject newInstance = new GameObject();
+                    newInstance.name = "Singleton[" + typeof(T).ToString() + "]";
+                    m_singleton = newInstance.AddComponent<T>();
+                }
+                if (m_singleton != this)
+                {
+                    Destroy(this.gameObject);
+                }
+            }
+        }
+    }
+}
