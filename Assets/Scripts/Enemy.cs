05b5a6576a91fc4749149483894e4e4ef6d602d7
NAMuniz521 <nickam521@gmail.com>
2020-07-11 22:41:51 -0500

    [Added] - Enemies, Bullets, Shooting Functionality

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("References")]
+    public Transform shootPoint;
+    public GameObject bullet;
+
+    [Header("Properties")]
+    public float fireInterval;
+    public float speed;
+    public Vector2 patrolPointA;
+    public Vector2 patrolPointB;
+
+    float tempTime;
+
+    private void Start()
+    {
+        tempTime = Time.time + fireInterval;
+    }
+
+    private void Update()
+    {
+        // TODO: make it move on a path
+
+        if (tempTime < Time.time)
+        {
+            Shoot();
+            tempTime = Time.time + fireInterval;
+        }
+    }
+
+    public void Shoot()
+    {
+        Debug.Log("pew pew");
+        //RaycastHit2D hitInfo = Physics2D.Raycast(shootPoint.position, -1 * shootPoint.right);
+
+        GameObject projectile = Instantiate(bullet);
+        projectile.transform.localPosition = shootPoint.transform.position;
+
+        //if (hitInfo)
+        //{
+        //    //Debug.Log(hitInfo.transform.name);
+        //}
+    }
+
+    public void Die()
+    {
+        // TODO: do any death effects or sound effects here
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [Header("References")]
+    public Rigidbody2D rb;
+
+    [Header("Properties")]
+    public float speed = 10f;
+    
+    void Start()
+    {
+        rb.velocity = -1 * transform.right * speed;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<CharacterController>() != null)
+        {
+            CharacterController player = collision.GetComponent<CharacterController>();
+
+        }
+        else if (collision.GetComponent<Enemy>() != null)
+        {
+            Enemy enemy = collision.GetComponent<Enemy>();
+            enemy.Die();
+        }
+        Destroy(gameObject);
+    }
+}
